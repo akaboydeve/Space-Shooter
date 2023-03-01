4aa97afa38d2768a7b966aae0c8994f9d231dbb9
akaboydeve <akashsardaryt@gmail.com>
2023-03-01 21:10:03 +0530

    initial commit

--- Assets/Enemy.cs
+++ Assets/Enemy.cs
@@ -6,13 +6,6 @@ public class Enemy : MonoBehaviour
 {
     [SerializeField]
     private float speed = 4;
-    // Start is called before the first frame update
-    void Start()
-    {
-
-    }
-
-    // Update is called once per frame
     void Update()
     {
         transform.Translate(Vector3.down * speed * Time.deltaTime);
